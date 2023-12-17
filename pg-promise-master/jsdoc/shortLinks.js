ea1b4f7e2cd2ed5274c1f2020f7da2f283ee6c38
peisakhovich <grigoriy.peisakhovlch@gmail.com>
2023-12-17 20:26:07 +0100

    commit delete two folders

--- pg-promise-master/jsdoc/shortLinks.js
+++
@@ -1,7 +0,0 @@
-const {fixLinks} = require('./fixLinks');
-
-exports.handlers = {
-    beforeParse(e) {
-        e.source = fixLinks(e.source);
-    }
-};
--- pg-promise-master/test/sql/params.sql
+++
@@ -1,7 +0,0 @@
-/*
-    This is to test static formatting parameters,
-    such as schema name and table name.
-*/
-
--- We schema/table we should use the SQL Name syntax (with tilde):
-SELECT ${column~} FROM ${schema~}.${table~}
